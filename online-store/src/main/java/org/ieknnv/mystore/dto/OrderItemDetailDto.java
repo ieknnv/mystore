13c90d75badd5e63d2cac3a33f265e403f610441
ieknn <ieknnv@gmail.com>
2025-09-07 19:03:31 +0200

    The project has been restructured to a multi module project

--- online-store/src/main/java/org/ieknnv/mystore/dto/OrderItemDetailDto.java
+++ online-store/src/main/java/org/ieknnv/mystore/dto/OrderItemDetailDto.java
@@ -2,17 +2,21 @@ package org.ieknnv.mystore.dto;
 
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
-public class CartItemDetailDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemDetailDto {
     private long id;
-    private long cartId;
+    private long orderId;
     private long itemId;
+    private long quantity;
+    private BigDecimal price;
     private String title;
     private String description;
-    private BigDecimal price;
-    private long quantity;
 }
