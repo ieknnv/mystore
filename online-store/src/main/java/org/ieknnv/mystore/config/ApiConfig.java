9f229a1ba90f5013235721627ed93447c8427d23
ieknn <ieknnv@gmail.com>
2025-10-10 14:52:00 +0200

    Dockerization

--- online-store/src/main/java/org/ieknnv/mystore/config/ApiConfig.java
+++ online-store/src/main/java/org/ieknnv/mystore/config/ApiConfig.java
@@ -2,19 +2,28 @@ package org.ieknnv.mystore.config;
 
 import org.ieknnv.payment.client.api.BalanceApi;
 import org.ieknnv.payment.client.api.PaymentApi;
+import org.ieknnv.payment.client.invoker.ApiClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiConfig {
 
+    @Value("${payment.server.url}")
+    private String paymentServerUrl;
+
     @Bean
     public BalanceApi balanceApi() {
-        return new BalanceApi();
+        var apiClient = new ApiClient();
+        apiClient.setBasePath(paymentServerUrl);
+        return new BalanceApi(apiClient);
     }
 
     @Bean
     public PaymentApi paymentApi() {
-        return new PaymentApi();
+        var apiClient = new ApiClient();
+        apiClient.setBasePath(paymentServerUrl);
+        return new PaymentApi(apiClient);
     }
 }
